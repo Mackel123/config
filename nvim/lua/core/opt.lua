6b5b34502230f0afcb19942afc1547b5cda3bf89
mackelzhang <mackel13411987729@outlook.com>
2024-05-06 04:44:09 +0800

    nvim lazygit

--- nvim/lua/core/opt.lua
+++ nvim/lua/core/opt.lua
@@ -27,3 +27,13 @@ g.nord_bold = true
 
 require('nord').set()
 
+--lazygit
+g.lazygit_floating_window_winblend = 1 -- transparency of floating window
+g.lazygit_floating_window_scaling_factor = 0.9 -- scaling factor for floating window
+g.lazygit_floating_window_border_chars = {'╭','─', '╮', '│', '╯','─', '╰', '│'} -- customize lazygit popup window border characters
+g.lazygit_floating_window_use_plenary = 0 -- use plenary.nvim to manage floating window if available
+g.lazygit_use_neovim_remote = 1 -- fallback to 0 if neovim-remote is not installed
+
+g.lazygit_use_custom_config_file_path = 0 -- config file path is evaluated if this value is 1
+g.lazygit_config_file_path = '' -- custom config file path
+
